06787c3914e89e78b7af89bf5127dc68bd34c6c9
Luka <lukacupic42@gmail.com>
2020-09-29 22:57:24 +0200

    Replace PNG with JPG images

--- scripts/stars.js
+++
@@ -1,6 +0,0 @@
-function drawStars() {
-    var canvas = document.getElementById("canvas");
-    var ctx = canvas.getContext("2d");
-    ctx.fillStyle = "blue";
-    ctx.fillRect(0, 0, canvas.width, canvas.height);
-}
